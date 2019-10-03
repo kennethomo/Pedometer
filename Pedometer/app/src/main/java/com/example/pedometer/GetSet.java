72baf72eaec4a07106728fc657efc6790638fa75
Kenneth <kennethomo830@gmail.com>
2019-10-02 20:22:32 -0500

    Update GetSet.java

--- Pedometer/app/src/main/java/com/example/pedometer/GetSet.java
+++ Pedometer/app/src/main/java/com/example/pedometer/GetSet.java
@@ -3,5 +3,11 @@ package com.example.pedometer;
 public class GetSet {
     private int numSteps = 0;
 
-    
+    public int getStep(){
+        return numSteps;
+    }
+
+    public int setStep(int newStep){
+        this.newStep = numSteps;
+    }
 }
